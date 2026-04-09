df336e5299211c96e10e0ba9d5db15130f1b2c82
tseeberCTR <tseeber.CTR@uncomn.com>
2026-04-09 13:58:26 -0700

    static site generator update

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import sys
 from textnode import markdown_to_html_node
 
 
@@ -28,7 +29,7 @@ def extract_title(markdown):
     raise Exception("No h1 header found in markdown")
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath="/"):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     
     with open(from_path, 'r') as f:
@@ -43,6 +44,8 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown_content)
     
     full_html = template_content.replace("{{ Title }}", title).replace("{{ Content }}", html_content)
+    full_html = full_html.replace('href="/', f'href="{basepath}')
+    full_html = full_html.replace('src="/', f'src="{basepath}')
     
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     
@@ -50,7 +53,7 @@ def generate_page(from_path, template_path, dest_path):
         f.write(full_html)
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath="/"):
     for item in os.listdir(dir_path_content):
         item_path = os.path.join(dir_path_content, item)
         dest_path = os.path.join(dest_dir_path, item)
@@ -58,17 +61,19 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         if os.path.isfile(item_path):
             if item.endswith('.md'):
                 dest_html_path = dest_path.replace('.md', '.html')
-                generate_page(item_path, template_path, dest_html_path)
+                generate_page(item_path, template_path, dest_html_path, basepath)
         else:
-            generate_pages_recursive(item_path, template_path, dest_path)
+            generate_pages_recursive(item_path, template_path, dest_path, basepath)
 
 
 def main():
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+    
     script_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     os.chdir(script_dir)
     
-    copy_files_recursive("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+    copy_files_recursive("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
 
 if __name__ == "__main__":
